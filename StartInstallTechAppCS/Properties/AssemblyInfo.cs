df883879037d9d3c495a027947b18e31943ae17a
Edward L Thomas <Edward@ThomasITServices.com>
2016-07-08 22:31:47 -0700

    still need to make a cmd starter and a network drive mapper

--- StartInstallTechAppCS/Properties/AssemblyInfo.cs
+++ StartInstallTechAppCS/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StartInstallTechAppCS")]
+[assembly: AssemblyTitle("Start Install TechApps")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StartInstallTechAppCS")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Thomas IT Services")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
